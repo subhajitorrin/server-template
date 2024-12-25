5cdb633f8d23b17b19024bcd4c45558751db6e0c
subhajitorrin <subhajitghosh343@gmail.com>
2024-12-26 00:33:44 +0530

    docker add

--- build/routes/route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_1 = require("../controllers/user");
-const router = (0, express_1.Router)();
-router.post("/user", user_1.addUser);
-router.get("/user", user_1.getAllUsers);
-exports.default = router;
